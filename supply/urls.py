d47006bf6df6d6935bf4df18ec6da9eb2bf1ab99
newtonkiragu <karanunewton4@gmail.com>
2020-03-03 00:59:24 +0300

    created url for signup

--- supply/urls.py
+++ supply/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-from .import views
+from . import views
 from django.conf import settings
-from django.conf.urls.static  import static
+from django.conf.urls.static import static
 
-urlpatterns=[
-    url('^$',views.index, name = 'mainPage'),
-    url('contact',views.contact ,name = 'contact'),
-    url('about',views.about ,name = 'about'),
+urlpatterns = [
+    url('^$', views.index, name='mainPage'),
+    url('contact', views.contact, name='contact'),
+    url('about', views.about, name='about'),
     url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^accounts/register/$', views.signup, name='signup'),
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) 
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
