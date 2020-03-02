5cf102fe64dbb6e3c039c6ec17f363b0bf15470e
newtonkiragu <karanunewton4@gmail.com>
2020-03-03 00:58:16 +0300

    updated views to add signup functionality

--- supply/views.py
+++ supply/views.py
@@ -1,31 +1,48 @@
-from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Image,Profile
-from django.contrib.auth.decorators import  login_required
+from .models import Image, Profile
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+
 
 # Create your views here.
 def contact(request):
-    return render( request,'contact.html')
+    return render(request, 'contact.html')
+
 
 def about(request):
-    return render( request,'about.html')
+    return render(request, 'about.html')
 
-@login_required(login_url='/accounts/login/')
+
 def index(request):
-
     images = Image.objects.all()
     users = User.objects.all()
 
-    return render (request, 'all-supply/index.html',{"images":images},)
+    return render(request, 'all-supply/index.html', {"images": images}, )
+
 
 @login_required(login_url='/accounts/login/')
-def profile(request,username):
+def profile(request, username):
     profile = User.objects.get(username=username)
-    
+
     try:
         profile_details = Profile.get_by_id(profile.id)
     except:
         profile_details = Profile.filter_by_id(profile.id)
-        
- 
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
