0d3fabb7cb53bae70cd50307a581f5e416471f6f
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 10:40:14 +0300

    created function to get totals for cart and order items

--- cart/models.py
+++ cart/models.py
@@ -14,6 +14,9 @@ class Cart(models.Model):
     def __str__(self):
         return f'{self.quantity} of {self.item.item_name}'
 
+    def get_total(self):
+        return self.item.item_price * self.quantity
+
 
 class Order(models.Model):
     order_items = models.ManyToManyField(Cart)
@@ -23,3 +26,10 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_totals(self):
+        total = 0
+        for order_item in self.order_items.all():
+            total += order_item.get_total()
+
+        return total
