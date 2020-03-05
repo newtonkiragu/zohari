9ead55f52b8b44fa84c279dcdb11de15a38ed21e
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 09:37:25 +0300

    renamed created at field

--- cart/models.py
+++ cart/models.py
@@ -9,7 +9,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.quantity} of {self.item.name}'
