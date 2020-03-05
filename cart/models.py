23f224df4d14eb80e92180af763d16ab818286a7
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 09:06:36 +0300

    created the order model

--- cart/models.py
+++ cart/models.py
@@ -13,3 +13,13 @@ class Cart(models.Model):
 
     def __str__(self):
         return f'{self.quantity} of {self.item.name}'
+
+
+class Order(models.Model):
+    order_items = models.ManyToManyField(Cart)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
