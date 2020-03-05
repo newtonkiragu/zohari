b59e7aa9ffa9817fd4bb131535161d544bb340c9
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 10:43:39 +0300

    fixed automatic login issue on signup

--- supply/views.py
+++ supply/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
