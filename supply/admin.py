27980040e4da8c2f1640883e44d87b3f9896a675
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 08:53:52 +0300

    registered brand and categories to admin

--- supply/admin.py
+++ supply/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product, Profile
+from .models import Product, Profile, Brand, Category
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Profile)
+admin.site.register(Brand)
+admin.site.register(Category)
