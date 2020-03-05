cd58c5464a1b779ae9ce3d014c39f9028e5ddf9b
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 10:41:44 +0300

    fixed error on templatetag to display ammount of stuff on cart

--- cart/templatetags/cart_total.py
+++ cart/templatetags/cart_total.py
@@ -9,6 +9,6 @@ def cart_total(user):
     order = Order.objects.filter(user=user, ordered=False)
 
     if order.exists():
-        return order[0].orderitems.count()
+        return order[0].order_items.count()
     else:
         return 0
