6f747ac84bd723fa0fcced06e7c85a0935a638ea
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 09:05:02 +0300

    created cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from supply.models import Product
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.item.name}'
