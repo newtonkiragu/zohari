fb5249b9beb5f503810efec51806cb5cb6755301
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 08:25:44 +0300

    updated product model to have item brand and item categories

--- supply/models.py
+++ supply/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
-    profile_photo = models.ImageField(default='default.jpg', upload_to='images/profiles/')
+    profile_photo = models.ImageField(default='images/profiles/default.jpg', upload_to='images/profiles/')
     bio = models.TextField(max_length=500,null=True, blank=True)
     phone_number = models.CharField(max_length=10, unique=True)
 
@@ -31,18 +31,35 @@ class Profile(models.Model):
         return profile
 
     def __str__(self):
-        return self.title
+        return self.user.username
 
 
 class Product(models.Model):
+    Brands_Choices = [
+        ('addidas', ('Addidas', 'images/brand/addidas.jpeg')),
+        ('under-armour', ('Under Armour', 'images/brand/under-armour.jpeg')),
+    ]
+    Product_Categories = [
+        ('balls', ('Balls', 'images/brand/balls.jpeg')),
+        ('rackets', ('Rackets', 'images/brand/rackets.jpeg')),
+        ('apparel', ('Apparel', 'images/brand/apparel.jpeg')),
+    ]
     images = models.ImageField(upload_to='images/product/')
     item_name = models.CharField(max_length=20)
     item_description = models.CharField(max_length=50)
     item_details = models.CharField(max_length=500)
     item_quantity = models.IntegerField(default=0)
     item_availability = models.BooleanField(default=False)
-    brand_name = models.CharField(max_length=20)
-    brand_image = models.ImageField(upload_to='images/brand/')
+    item_brand = models.CharField(
+        max_length=1,
+        choices=Brands_Choices,
+        null=True
+    )
+    item_categories = models.CharField(
+        max_length=1,
+        choices=Product_Categories,
+        null=True
+    )
 
     def __str__(self):
         return self.item_name
