a2df81c3f9a5cb438b4a12c40d8cbf4b3da40cf0
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 07:34:55 +0300

    created profile and product models

--- supply/models.py
+++ supply/models.py
@@ -6,17 +6,15 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     profile_photo = models.ImageField(default='default.jpg', upload_to='images/profiles/')
-    # bio = models.TextField(max_length=500,default='Tell Me Something')
-    # website = models.CharField(max_length=10, blank=True,default='me.com')
-    email = models.EmailField(max_length=200)
-    phone_number = models.CharField(max_length=10, null=True, blank=True)
+    bio = models.TextField(max_length=500,null=True, blank=True)
+    phone_number = models.CharField(max_length=10, unique=True)
 
     @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
         if created:
-            Profile.objects.create(instance)
+            Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
     def save_profile(sender, instance, **kwargs):
@@ -41,6 +39,7 @@ class Product(models.Model):
     item_name = models.CharField(max_length=20)
     item_description = models.CharField(max_length=50)
     item_details = models.CharField(max_length=500)
+    item_quantity = models.IntegerField(default=0)
     item_availability = models.BooleanField(default=False)
     brand_name = models.CharField(max_length=20)
     brand_image = models.ImageField(upload_to='images/brand/')
@@ -52,4 +51,4 @@ class Product(models.Model):
         return self.save()
 
     class Meta:
-        ordering = ['images']
+        ordering = ['item_name']
