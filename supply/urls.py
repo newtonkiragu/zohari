d6219a8b90f03b8d862c803e22c3913e8e9a167b
newtonkiragu <karanunewton4@gmail.com>
2020-03-05 09:38:14 +0300

    registered cart urisl to add and remove item from cart

--- supply/urls.py
+++ supply/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+
+from cart.views import add_to_cart, remove_from_cart
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +11,8 @@ urlpatterns = [
     url('about', views.about, name='about'),
     url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
     url(r'^accounts/register/$', views.signup, name='signup'),
+    url('cart/(?P<slug>\w+)', add_to_cart, name='cart'),
+    url('remove/(?P<slug>\w+)', remove_from_cart, name='remove-cart'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
