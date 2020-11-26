3a8ace60669ad5235272717fb16949a42f8ceadb
newtonkiragu <karanunewton4@gmail.com>
2020-11-26 18:05:37 +0300

    deleted db and made migrations

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from shop.models import Brand, Category, Images, Product
 # Register your models here.
+
+admin.site.Register(Brand)
+admin.site.Register(Category)
+admin.site.Register(Images)
+admin.site.Register(Product)
