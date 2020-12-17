35317c31416a13a93374068c58450a838306efa9
newtonkiragu <karanunewton4@gmail.com>
2020-12-17 14:18:14 +0300

    finally fixed routing bugs

--- shop/urls.py
+++ shop/urls.py
@@ -8,17 +8,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.HomePageView.as_view(), name='landing_page'),
-    url('contact', views.contact, name='contact'),
-    url('about', views.about, name='about'),
-    url(r'^profile/(?P<username>\w+)$', views.profile, name='profile'),
-    url(r'^accounts/register/$', views.SignUpView.as_view(), name='signup'),
-    url('^cart/(?P<slug>.+)$', add_to_cart, name='add-to-cart'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('accounts/register/', views.SignUpView.as_view(), name='signup'),
+    path('cart/<slug>/', add_to_cart, name='add-to-cart'),
     # url('^cart/$', add_to_cart, name='cart'),
-    # url(r'product/(?P<slug>.+)$', views.product_detail, name='product-detail'),
-    url('cart/decrease/(?P<slug>.+)$', decrease_cart, name='decrease-cart'),
-    url(r'^remove/(?P<slug>.+)$', remove_from_cart, name='remove-cart'),
-    url('search/', views.SearchResultsView.as_view(), name='search_results'),
-    url('shop/', views.ProductListView.as_view(), name="product-list"),
+    path('product/<slug>/', views.product_detail, name='product-detail'),
+    path('cart/decrease/<slug>/', decrease_cart, name='decrease-cart'),
+    path('remove/<slug>/', remove_from_cart, name='remove-cart'),
+    path('search/', views.SearchResultsView.as_view(), name='search_results'),
+    path('shop/', views.ProductListView.as_view(), name="product-list"),
     path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
 ]
 
